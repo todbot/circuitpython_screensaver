ad95d0dfead764f24570917d876cd0dc75302b7f
Tod Kurt <todbot@users.noreply.github.com>
2021-08-19 08:48:59 -0700

    add jp200 boingball example

---
+++ examples/jp200_code.py
@@ -0,0 +1,9 @@
+# jp200_code.py -- screensaver celbebration for JP's 200th show
+# This is designed for FunHouse but should work on other boards
+# 19 Aug 2021
+
+from screensaver import screensaver_boingball
+
+screensaver_boingball(bg_fname="bg_jp200.bmp")
+
+
